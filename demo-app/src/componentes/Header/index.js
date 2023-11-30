c78b5c884d774c4a30fba16de1df43936c1590a4
rodrigospm <rodrigo.perez@ucsp.edu.pe>
2023-11-30 08:37:11 -0500

    demo-app

---
+++ demo-app/src/componentes/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nike from "../../images/Nike.jpg";
+
+export const Header = () => {
+  return (
+    <header>
+        <div className='menu'>
+            <box-icon name="menu"> </box-icon>
+        </div>
+        <a href='#'>
+            <div className='logo'>
+                <img src={Nike} alt=""/>
+            </div>
+        </a>
+    </header>
+  )
+}
