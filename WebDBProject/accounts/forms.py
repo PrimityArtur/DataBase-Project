1c9c76c2044c4078b63985227be13eaf3036ef8b
PrimityArtur <primityartur@gmail.com>
2023-12-03 03:52:14 -0500

    Backend completado el llenado de register.html

--- WebDBProject/accounts/forms.py
+++ WebDBProject/accounts/forms.py
@@ -4,4 +4,15 @@ class Login(forms.Form):
     correo = forms.CharField(label="correo", max_length=200)
     contrasena = forms.CharField(label="contrasena", max_length=200)
     
+
+class Register(forms.Form):
+    correo = forms.CharField(label="correo", max_length=100)
+    contrasena = forms.CharField(label="contrasena", max_length=100)
+
+    nombre = forms.CharField(label="nombre", max_length=200)
+    apellido = forms.CharField(label="apellido", max_length=200)
+
+    direccion = forms.CharField(label="direccion", max_length=200, required=False)
+    telefono = forms.CharField(label="telefono", max_length=200, required=False)
+    
     
--- WebDBProject/accounts/views.py
+++ WebDBProject/accounts/views.py
@@ -26,9 +26,9 @@ def login(request):
                 """, 
                 {'1': correo, '2': contrasena})
             
-            resultado = cursor.fetchone()
+            usuario_id = cursor.fetchone()
 
-            if(resultado):
+            if(usuario_id):
                 cursor = connection.cursor().execute("""
                         SELECT 
                             p.PRODUCTO_ID,
@@ -68,11 +68,122 @@ def login(request):
                 
                 return render(request, 'index.html', 
                         {'productos': Productos, 
-                        'usuario_id': resultado[0]
+                        'usuario_id': usuario_id[0]
                         })
         
     return render(request, 'login.html', 
                   {'form': form})
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        #show interface
+        form = forms.Register()
+    
+        return render(request, 'register.html', 
+                  {'form': form})
+    
+    elif request.method == 'POST':
+        form = forms.Register(request.POST)
+        if form.is_valid():
+            correo = form.cleaned_data['correo']
+            contrasena = form.cleaned_data['contrasena']
+            nombre = form.cleaned_data['nombre']
+            apellido = form.cleaned_data['apellido']
+            direccion = form.cleaned_data['direccion']
+            telefono = form.cleaned_data['telefono']
+
+            cursor = connection.cursor().execute("""
+                SELECT
+                    USUARIO_ID
+                FROM USUARIO 
+                WHERE CORREO = :1 AND CONTRASENA = :2   
+                """, 
+                {'1': correo, '2': contrasena})
+            
+            usuario_id = cursor.fetchone()
+
+            if(usuario_id):
+                return render(request, 'login.html', 
+                  {'form': forms.Login()})
+            else:
+
+                connection.cursor().execute("""
+                        INSERT INTO USUARIO(NOMBRE, APELLIDO, CORREO, CONTRASENA) VALUES (:1 , :2, :3, :4)
+                    """, {
+                        '1' : nombre, 
+                        '2' : apellido,
+                        '3' : correo,
+                        '4' : contrasena 
+                    })
+                
+                cursor = connection.cursor().execute("""
+                        SELECT
+                            USUARIO_ID
+                        FROM USUARIO 
+                        WHERE CORREO = :1 AND CONTRASENA = :2   
+                        """, 
+                        {'1': correo, '2': contrasena})
+                usuario_id = cursor.fetchone()
+
+                connection.cursor().execute("""
+                        INSERT INTO PERFIL(USUARIO_ID, DIRECCION, TELEFONO) VALUES (:1 , :2, :3)
+                    """, {
+                        '1' : usuario_id[0], 
+                        '2' : direccion, 
+                        '3' : telefono 
+                    })
+                
+                connection.cursor().execute("""
+                        INSERT INTO COMPRADOR(USUARIO_ID) VALUES (:1)
+                    """, {
+                        '1' : usuario_id[0]
+                    })
+                
+                connection.cursor().execute("""
+                        INSERT INTO VENDEDOR(USUARIO_ID) VALUES (:1)
+                    """, {
+                        '1' : usuario_id[0]
+                    })
+                
+                cursor = connection.cursor().execute("""
+                        SELECT 
+                            p.PRODUCTO_ID,
+                            p.NOMBRE,
+                            p.PRECIO,
+                            t.TIPO,
+                            AVG(val.ESTRELLAS) AVG_ESTRELLAS,
+                            COUNT(d.COMPRADOR_ID) AVG_DESCARGAS,
+                            vu.NOMBRE
+
+                        FROM PRODUCTO p 
+                        JOIN TIPO t 
+                        ON (p.PRODUCTO_ID = t.PRODUCTO_ID)
+                        JOIN DESCARGA d 
+                        ON (p.PRODUCTO_ID = d.PRODUCTO_ID)
+                        JOIN VALORACION val
+                        ON (p.PRODUCTO_ID = val.PRODUCTO_ID)
+                        JOIN VENDEDOR ven
+                        ON (p.VENDEDOR_ID = ven.VENDEDOR_ID)
+                        JOIN USUARIO vu
+                        ON (ven.USUARIO_ID = vu.USUARIO_ID)
+
+                        GROUP BY p.PRODUCTO_ID, p.NOMBRE, p.PRECIO, t.TIPO, vu.NOMBRE
+                    """)
+                
+                resultados = cursor.fetchall()    
+                Productos = [{
+                        'producto_id': producto_id, 
+                        'nombre': nombre, 
+                        'precio': precio,
+                        'tipo' : tipo,
+                        'estrellas' : estrellas,
+                        'descargas' : descargas,
+                        'vendedor' : vendedor
+                        } 
+                        for producto_id, nombre, precio, tipo, estrellas, descargas, vendedor in resultados]
+                
+                return render(request, 'index.html', 
+                        {'productos': Productos, 
+                        'usuario_id': usuario_id[0]
+                        })
+        
